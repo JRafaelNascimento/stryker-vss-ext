f7adfd4ab81d188f14d3eacc44cf5112cf580ac1
Rafael Nascimento <joao.bezerra@stone.com.br>
2022-09-21 10:57:30 -0300

    Updates index.ts

--- src/strykersolutiontest/index.ts
+++ src/strykersolutiontest/index.ts
@@ -1,6 +1,6 @@
 import * as tl from 'azure-pipelines-task-lib/task';
 
-const strykerAppPython: string  = "https://gist.githubusercontent.com/ThiagoBarradas/bc845ca918be6002a707819806c548fe/raw";
+const strykerAppPython: string  = "https://gist.githubusercontent.com/JRafaelNascimento/3a6146b6e4d0a4db411ff4813f92e74b/raw/546ea1c876bf497f8f81331e20cd5936867aaf68/stryker-solution-script.py";
 
 async function run() {
     try {
@@ -8,10 +8,10 @@ async function run() {
         var configFilePath: string = tl.getInput('configFilePath', true);
 
         // install tools
-        await tl.exec('wget', ["-O", "app.py", strykerAppPython]);
-        
+        await tl.exec('wget', ["-O", "stryker.py", strykerAppPython]);
+
         // execute integration tests
-        await tl.exec('python3', ["app.py", "--config-file", configFilePath]);
+        await tl.exec('python3', ["stryker.py", "--config-file", configFilePath]);
     }
     catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message);
